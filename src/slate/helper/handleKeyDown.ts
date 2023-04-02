c9be19a54e8c146b025abb2b046e49d354413993
linqirong <609413692@qq.com>
2023-04-03 01:20:40 +0800

    feat:增加跳出代码块功能

--- src/slate/helper/handleKeyDown.ts
+++ src/slate/helper/handleKeyDown.ts
@@ -1,4 +1,4 @@
-import { Editor, Range, Element, Text } from 'slate'
+import { Editor, Range, Element, Text, Transforms, Path } from 'slate'
 import { CustomEditor } from '../../types/slate'
 
 export const handleKeyDown = (
@@ -8,6 +8,36 @@ export const handleKeyDown = (
   if (e.key === 'Enter' && e.shiftKey) {
     e.preventDefault()
     Editor.insertText(editor, '\n')
+  }
+  // commend+enter跳出当前的块
+  if (e.key === 'Enter' && e.metaKey) {
+    const { selection } = editor
+    if (selection && Range.isCollapsed(selection)) {
+      const [match] = Editor.nodes(editor, {
+        match: n =>
+          !Editor.isEditor(n) &&
+          Element.isElement(n) &&
+          (n.type === 'list-item' || n.type === 'code-line')
+      })
+      if (match) {
+        const [_li, liPath] = match
+        const [_ul, ulPath] = Editor.parent(editor, liPath)
+        const hasNext = Editor.hasPath(editor, Path.next(ulPath))
+        if (!hasNext) {
+          Transforms.insertNodes(
+            editor,
+            {
+              type: 'paragraph',
+              children: []
+            },
+            {
+              at: Path.next(ulPath)
+            }
+          )
+        }
+        Transforms.select(editor, Path.next(ulPath))
+      }
+    }
   }
   if (e.key === 'Enter') {
     const { selection } = editor
