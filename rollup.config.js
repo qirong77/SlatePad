030cda87e1048dfa31b601f1f1ec41ddbf2c4754
linqirong <609413692@qq.com>
2023-04-21 02:37:39 +0800

    测试打包tailwindcss

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,17 @@ import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
+import postcss from 'rollup-plugin-postcss'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+import postcssImport from 'postcss-import'
+import tailwindConfig from './tailwind.config.js'
 export default {
   input: 'src/slate/SlatePad.tsx',
   output: {
-    file: 'package/index.js',
+    file: 'package/dist/index.js',
     format: 'es',
+    assetFileNames: 'asset.css',
     sourcemap: true
   },
   plugins: [
@@ -17,7 +23,17 @@ export default {
       babelHelpers: 'runtime',
       exclude: 'node_modules/**',
       presets: ['@babel/preset-react']
-    })
+    }),
+    postcss({
+      extract: true,
+      extensions: ['.css'],
+      plugins: [
+
+        tailwindcss(tailwindConfig)
+      ]
+    }),
+
+
   ],
   external: ['react', 'react-dom']
 }
