8fb95eaa7e074e8abf2f04f33e183fa5e413e912
linqirong <609413692@qq.com>
2023-04-25 14:31:13 +0800

    fix:markdown功能太复制,暂时放弃

--- src/slate/utils/index.ts
+++ src/slate/utils/index.ts
@@ -1,7 +1,5 @@
 import { CustomEditor } from './../../types/slate'
 import { Transforms, Node } from 'slate'
-import { marked, Renderer } from 'marked'
-import { deserialize } from '../plugins/withPastHtml'
 import { slateToMarkdown } from './slateToMarkdown'
 
 function clearAll(editor: CustomEditor) {
@@ -19,20 +17,10 @@ function replaceAll(editor: CustomEditor, fragment: Node[]) {
   clearAll(editor)
   editor.insertFragment(fragment)
 }
-function insertMarkdown(editor: CustomEditor, markdownString = '') {
-  const renderer: Renderer = new marked.Renderer()
-  renderer.options.breaks = true
-  const htmlString = marked(markdownString, { renderer })
-  const html = document.createElement('body')
-  html.innerHTML = htmlString
-  const fragment = deserialize(html).filter((node:any) => node.type)
-  Transforms.insertFragment(editor, fragment)
-}
 
 export const RichUtils = {
   clearAll,
   clearHistory,
   replaceAll,
-  insertMarkdown,
   slateToMarkdown
 }
