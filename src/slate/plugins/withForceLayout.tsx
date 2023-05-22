9bb53063ea27c7fa9ef07dd5fbd059b72fc4a953
linqirong <609413692@qq.com>
2023-05-22 23:46:50 +0800

    fix:完善forceLayout逻辑

--- src/slate/plugins/withForceLayout.tsx
+++ src/slate/plugins/withForceLayout.tsx
@@ -3,59 +3,64 @@ import { Editor, Node, Path, Transforms, Element, NodeEntry } from 'slate'
 // normalize只有在节点变化的时候会执行
 export const withForceLayout = (editor: CustomEditor) => {
   const { normalizeNode } = editor
+  // 如果第一个元素,直接是个表格,这里有个bug
+  // setTimeout(() => {
+  //   forceLayout(editor)
+  // }, 100)
+  // 后面对编辑器输入的时候,会执行
   editor.normalizeNode = entry => {
     if (!entry[1].length) {
-      // 强制最后一个元素为段落
-      const path = Editor.end(editor, [])
-      const block = Editor.above(editor, {
-        match: n =>
-          Element.isElement(n) &&
-          Editor.isBlock(editor, n) &&
-          n.type !== 'list-item' &&
-          n.type !== 'code-line',
-        at: path
-      }) as NodeEntry<SlateElement>
-      if (block) {
-        const [lastBlock, lastPath] = block
-        if (lastBlock.type === 'paragraph' && !Node.string(lastBlock).length) {
-          return
-        }
-        Transforms.insertNodes(
-          editor,
-          {
-            type: 'paragraph',
-            children: [{ text: '' }]
-          },
-          {
-            at: Path.next(lastPath)
-          }
-        )
-      }
-      // 强制第一个为fixSelect
-      const startPath = Editor.start(editor, [])
-      const [startBlock] =
-        (Editor.above(editor, {
-          match: n =>
-            Element.isElement(n) &&
-            Editor.isBlock(editor, n) &&
-            n.type !== 'list-item' &&
-            n.type !== 'code-line',
-          at: startPath
-        }) as NodeEntry<SlateElement>) || []
-      if (startBlock?.type !== 'fix-select') {
-        Transforms.insertNodes(
-          editor,
-          {
-            type: 'fix-select',
-            children: [{ text: '' }]
-          },
-          { at: [0] }
-        )
-      }
+      forceLayout(editor)
     }
 
     return normalizeNode(entry)
   }
 
   return editor
+}
+
+// 强制最后一个元素为段落
+function forceLayout(editor: CustomEditor) {
+  const endPath = Editor.end(editor, [])
+  const lastBlockType = editor.children.at(-1)?.type || ''
+  const block = Editor.above(editor, {
+    match: node =>
+      Element.isElement(node) && Editor.isBlock(editor, node) && node.type === lastBlockType,
+    at: endPath
+  }) as NodeEntry<SlateElement>
+  if (block) {
+    const [lastBlock, lastPath] = block
+    if (lastBlock.type === 'paragraph' && !Node.string(lastBlock).length) {
+      return
+    }
+    Transforms.insertNodes(
+      editor,
+      {
+        type: 'paragraph',
+        children: [{ text: '' }]
+      },
+      {
+        at: Path.next(lastPath)
+      }
+    )
+  }
+  // 强制第一个为fixSelect
+  const startPath = Editor.start(editor, [])
+  const firstBlockType = editor.children.at(-1)?.type || ''
+  const [startBlock] =
+    (Editor.above(editor, {
+      match: node =>
+        Element.isElement(node) && Editor.isBlock(editor, node) && node.type === firstBlockType,
+      at: startPath
+    }) as NodeEntry<SlateElement>) || []
+  if (startBlock?.type !== 'fix-select') {
+    Transforms.insertNodes(
+      editor,
+      {
+        type: 'fix-select',
+        children: [{ text: '' }]
+      },
+      { at: [0] }
+    )
+  }
 }
