c8c69bb4cf230c6fb96cbf46a946f5580eddd0fb
linqirong <609413692@qq.com>
2023-05-25 23:18:13 +0800

    docs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { createSlatepad } from './slate/plugins/editor'
 import 'prism-themes/themes/prism-one-light.css'
 import { initialValue } from './common/const'
 import { GitHubIcon } from './assets/svg/icon'
-
 export const App = () => {
   const editor = useMemo(() => createSlatepad(), [])
   return (
@@ -12,7 +11,13 @@ export const App = () => {
       <GitHubIcon />
       <h1 className="text-center text-4xl my-[20px]">SlatePad</h1>
       <main className="w-[90vw] h-[80vh]">
-        <SlatePad editor={editor} initialValue={initialValue} />
+        <SlatePad
+          editor={editor}
+          onChange={value => {
+            // console.log(value)
+          }}
+          initialValue={initialValue}
+        />
       </main>
     </div>
   )
